9f1a164900b4201e491cae7583a15f34b6d044d0
Dan Putman <putman.dan@gmail.com>
2019-10-30 15:50:39 -0400

    Add Google Truth for assertions

--- src/test/java/api/controller/TodoControllerTest.java
+++ src/test/java/api/controller/TodoControllerTest.java
@@ -1,5 +1,7 @@
 package api.controller;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,6 +9,7 @@ public class TodoControllerTest {
 
     @Test
     public void putTodoReturnsInputPlus1() {
+        assertThat("awesome").startsWith("awe");
         Assert.assertEquals(new TodoController().putTodo(1), 2);
     }
 }
